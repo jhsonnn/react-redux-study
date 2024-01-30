737d6f5ea00895c51f87eb1c212b6563bf91e5ba
jhsonnn <jihyeongson13@gmail.com>
2024-01-30 18:13:20 +0900

    row 컴포넌트 생성하기

--- react-apple-app/src/App.jsx
+++ react-apple-app/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css';
 import styled from 'styled-components';
 import Nav from './components/MovieModal/Nav';
 import Banner from './components/MovieModal/Banner';
+import Row from './components/MovieModal/Row';
+import requests from './api/request';
 
 function App() {
 
@@ -9,6 +11,10 @@ function App() {
     <Container>
       <Nav />
       <Banner />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" id="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" id="CM" fetchUrl={requests.fetchComedyMovies} />
     </Container>
   )
 }
--- react-apple-app/src/components/MovieModal/Row.jsx
+++ react-apple-app/src/components/MovieModal/Row.jsx
@@ -1,9 +1,48 @@
 import './Row.css';
+import axios from '../../api/axios';
+import { useCallback, useEffect, useState } from 'react';
 
-const Row = () => {
+const Row = ({title, id, fetchUrl}) => {
+
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovieData = useCallback(async () => {
+    const response = await axios.get(fetchUrl);
+    //[{},{},{},...]
+    setMovies(response.data.results);
+  }, [fetchUrl])
+  
+  //영화 정보를 가져오기
+  useEffect(()=>{
+    fetchMovieData();
+  }, [fetchMovieData])
+  
+  
   return (
     <div>
-      
+      <h2>{title}</h2>
+      <div className='slider'>
+        <div className='slider__arrow-left'>
+          <span className='arrow'>
+            {"<"}
+          </span>
+        </div>
+        <div id={id} className='row__posters'>
+          {movies.map((movie)=>(
+            <img 
+              key={movie.id}
+              className='row__poster'
+              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className='slider__arrow-right'>
+          <span className='arrow'>
+            {">"}
+          </span>
+        </div>
+      </div>
     </div>
   )
 }
