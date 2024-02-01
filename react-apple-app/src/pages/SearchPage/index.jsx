0b8fa017aa4934d93d7beaa0e85ebf58e6aaea53
jhsonnn <jihyeongson13@gmail.com>
2024-02-01 11:08:02 +0900

    검색페이지 UI 구현 & 스타일링

--- react-apple-app/src/pages/SearchPage/index.jsx
+++ react-apple-app/src/pages/SearchPage/index.jsx
@@ -1,10 +1,12 @@
+import "./SearchPage.css";
 import axios from "../../api/axios";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom"
+import { useLocation, useNavigate } from "react-router-dom"
 
 const SearchPage = () => {
 
   const [searchResults, setSearchResults ]= useState([]);
+  const navigate = useNavigate();
 
   // const location = useLocation();
   // console.log(location);
@@ -34,11 +36,43 @@ const SearchPage = () => {
     }
   }
 
-  return (
-    <div>
-      SearchPage
-    </div>
-  )
+  if(searchResults.length > 0){
+    return (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if(movie.backdrop_path !== null && movie.media_type !== "person"){
+            const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return(
+              <div className="movie" key={movie.id}>
+                <div
+                  onClick={()=> navigate(`/${movie.id}`)}
+                  className="movie__column-poster"
+                >
+                  <img 
+                    src={movieImageUrl}
+                    alt="movie"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            )
+          }
+        } )}
+      </section>
+    )
+  }else {
+    return(
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>
+            찾고자하는 검색어 {searchTerm} 에 맞는 영화가 없습니다.
+          </p>
+        </div>
+      </section>
+    )
+  }
+
+  
 }
 
 export default SearchPage
