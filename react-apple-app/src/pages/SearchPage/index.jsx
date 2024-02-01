87110eede37bb9781d744f9fa1acd8d203a1ecbe
jhsonnn <jihyeongson13@gmail.com>
2024-02-01 10:36:04 +0900

    검색페이지 구현

--- react-apple-app/src/pages/SearchPage/index.jsx
+++ react-apple-app/src/pages/SearchPage/index.jsx
@@ -1,5 +1,39 @@
+import axios from "../../api/axios";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom"
 
 const SearchPage = () => {
+
+  const [searchResults, setSearchResults ]= useState([]);
+
+  // const location = useLocation();
+  // console.log(location);
+
+  const useQuery = ()=>{
+    return new URLSearchParams(useLocation().search);
+  }
+
+  let query = useQuery();
+  const searchTerm = query.get('q');
+
+  useEffect(()=>{
+    if(searchTerm){
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm])
+
+  const fetchSearchMovie = async (searchTerm) =>{
+    try{
+      const response = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      )
+      console.log(response)
+      setSearchResults(response.data.results);
+    }catch (error){
+      console.log(error);
+    }
+  }
+
   return (
     <div>
       SearchPage
