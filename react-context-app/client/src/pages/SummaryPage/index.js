504be3fa871d855eff1ec918efe943a95722d985
jhsonnn <jihyeongson13@gmail.com>
2024-02-03 23:37:55 +0900

    주문 페이지 UI 생성하기

--- react-context-app/client/src/pages/SummaryPage/index.js
+++ react-context-app/client/src/pages/SummaryPage/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SummaryPage = () => {
+  const [checked, setChecked ]= useState(false);
+
   return (
     <div>
-      
+      <form>
+        <input
+          type="checkbox"
+          checked={checked}
+          id="confirm-checkbox"
+          // 클릭할 때마다 input 변경
+          onClick={(e)=> setChecked(e.target.checked)}
+        />{" "}
+        <label htmlFor='confirm-checkbox'>
+          주문하려는 것을 확인하셨나요?
+        </label>
+        <br />
+        {/* checkbox 클릭했을 때만 버튼이 활성화 */}
+        <button disabled={!checked} type="submit">
+          주문 확인
+        </button>
+      </form>
     </div>
   )
 }
 
-export default index
+export default SummaryPage
