fb8b60ace675b77f6f3623832e5bbe86cfff3288
jhsonnn <jihyeongson13@gmail.com>
2024-02-05 16:36:36 +0900

    step 이용하여 페이지 나누기
    totalPrice 계산

--- react-context-app/client/src/App.js
+++ react-context-app/client/src/App.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import OrderPage from './pages/OrderPage';
 import SummaryPage from './pages/SummaryPage';
+import CompletePage from './pages/CompletePage';
 
 function App() {
+  const [step, setStep] = useState(0);
   return (
     <div style={{padding: '4rem'}}>
-      <OrderPage />
-      <SummaryPage />
+      {/* step이 바뀔때마다 업데이트 해줘야하므로 setStep도 내려줘야함 */}
+      {step === 0 && <OrderPage setStep={setStep} /> }
+      {step === 1 && <SummaryPage setStep={setStep} /> }
+      {step === 2 && <CompletePage setStep={setStep} /> }
     </div>
   );
 }
