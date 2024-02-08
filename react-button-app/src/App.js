1c3f24bbc14e5632641795a11ee3b22413470a3a
jhsonnn <jihyeongson13@gmail.com>
2024-02-08 18:25:22 +0900

    on/off 버튼 disbled
    fireEvent, disabled 세팅

--- react-button-app/src/App.js
+++ react-button-app/src/App.js
@@ -4,20 +4,31 @@ import './App.css';
 function App() {
 
   const [count, setCount] = useState(0);
+  const [disabled, setDistabled] = useState(false);
 
   return (
     <div className="App">
       <header className="App-header">
         <h3 data-testid="counter">{count}</h3>
-        <button data-testid="minus-button" onClick={()=>setCount(prev=>prev-1)}>
+        <button
+          data-testid="minus-button"
+          onClick={()=>setCount(prev=>prev-1)}
+          disabled={disabled}
+        >
           -
           </button>
-        <button data-testid="plus-button" onClick={()=>setCount(prev => prev+1)}>
+        <button
+          data-testid="plus-button" 
+          onClick={()=>setCount(prev => prev+1)}
+          disabled={disabled}
+        >
           +
         </button>
         <div>
-          <button style={{backgroundColor: 'blue' }}
+          <button
+            style={{backgroundColor: 'blue' }}
             data-testid="on/off-button"
+            onClick={()=>setDistabled(prev=> !prev)}
           >
             on/off
           </button>
