6fe6bffad6d4a601f915d1f42f582d6907794415
jhsonnn <jihyeongson13@gmail.com>
2024-02-08 18:04:08 +0900

    on/off 버튼 만들기 (toHaveStyle)

--- react-button-app/src/App.js
+++ react-button-app/src/App.js
@@ -15,6 +15,13 @@ function App() {
         <button data-testid="plus-button" onClick={()=>setCount(prev => prev+1)}>
           +
         </button>
+        <div>
+          <button style={{backgroundColor: 'blue' }}
+            data-testid="on/off-button"
+          >
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
